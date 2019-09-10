8b413d4b59468743eabc67beeaa38ad0ee7c2bb6
MaheshaMuthumala <31380743+MaheshaMuthumala@users.noreply.github.com>
2019-09-10 20:21:35 +0530

    commit-1

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Trim_Words
+{
+    class Program
+    {
+        public static int SpaceCount(string str)
+	{   
+		int spaceCount = 0;
+		string inputString;
+		for (int i = 0;i < str.Length;i++)
+			{
+			  inputString = str.Substring(i,1);
+			  if (inputString == " ")
+					spaceCount++;
+			}
+		return spaceCount;
+	}
+        static void Main(string[] args)
+        {
+            string val;
+            Console.Write("Enter String: ");
+            val = Console.ReadLine();
+            //Console.WriteLine(val);
+            string trim = Regex.Replace( val, @"\s", "" );
+            int strLength = val.Length;
+            int count = SpaceCount(val);
+
+            Console.WriteLine("Lenth of the String: " + strLength);
+            Console.WriteLine("Number of Spaces:" + count);
+            Console.WriteLine("Trimmed String: " + trim);
+        }
+    }
+}
